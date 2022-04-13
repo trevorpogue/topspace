trevorpogue/topspace

commit c326169190bac3737a33e5110c1a77a7842e38f5

Fix inability to use scrolling commands interactively (#11)

topspace.el
@@ -216,9 +216,10 @@ TOTAL-LINES is used in the same way as in `scroll-down'."
 (defun topspace--filter-args-scroll-down (&optional total-lines)
   "Run before `scroll-down' for scrolling above the top line.
 TOTAL-LINES is used in the same way as in `scroll-down'."
+  (setq total-lines (car total-lines))
   (cond
    ((not (topspace--enabled)) total-lines)
-   ((setq total-lines (car total-lines))
+   (t
     (setq total-lines (or total-lines (- (topspace--window-height)
                                          next-screen-context-lines)))
     (setq topspace--total-lines-scrolling total-lines)
@@ -227,9 +228,10 @@ TOTAL-LINES is used in the same way as in `scroll-down'."
 (defun topspace--filter-args-scroll-up (&optional total-lines)
   "Run before `scroll-up' for scrolling above the top line.
 TOTAL-LINES is used in the same way as in `scroll-up'."
+  (setq total-lines (car total-lines))
   (cond
    ((not (topspace--enabled)) total-lines)
-   ((setq total-lines (car total-lines))
+   (t
     (setq total-lines (* (or total-lines (- (topspace--window-height)
                                             next-screen-context-lines)) -1))
     (setq topspace--total-lines-scrolling total-lines)

.gitignore
@@ -1,0 +1,1 @@
+*.~undo-tree~

commit e3cd398678ca16328f708aeaa3419ef6a436c7c8

Fix bug caused by #11 when scrolling with topspace disabled
- #11 caused a bug where is was not possible to scroll anymore if
  topspace is disabled in the current buffer. This resolves that issue.

topspace.el
@@ -216,10 +216,10 @@ TOTAL-LINES is used in the same way as in `scroll-down'."
 (defun topspace--filter-args-scroll-down (&optional total-lines)
   "Run before `scroll-down' for scrolling above the top line.
 TOTAL-LINES is used in the same way as in `scroll-down'."
-  (setq total-lines (car total-lines))
   (cond
    ((not (topspace--enabled)) total-lines)
    (t
+    (setq total-lines (car total-lines))
     (setq total-lines (or total-lines (- (topspace--window-height)
                                          next-screen-context-lines)))
     (setq topspace--total-lines-scrolling total-lines)
@@ -228,10 +228,10 @@ TOTAL-LINES is used in the same way as in `scroll-down'."
 (defun topspace--filter-args-scroll-up (&optional total-lines)
   "Run before `scroll-up' for scrolling above the top line.
 TOTAL-LINES is used in the same way as in `scroll-up'."
-  (setq total-lines (car total-lines))
   (cond
    ((not (topspace--enabled)) total-lines)
    (t
+    (setq total-lines (car total-lines))
     (setq total-lines (* (or total-lines (- (topspace--window-height)
                                             next-screen-context-lines)) -1))
     (setq topspace--total-lines-scrolling total-lines)